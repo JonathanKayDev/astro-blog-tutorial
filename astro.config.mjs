72fc9994707806dd5189279787d86e5ecf519cf4
Jonathan Kay <67706563+JonathanKayDev@users.noreply.github.com>
2024-06-17 10:19:42 +0100

    Lesson 14 - Does not build

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,15 @@
 import { defineConfig } from 'astro/config';
 import icon from "astro-icon";
 
+import sitemap from "@astrojs/sitemap";
+
 // https://astro.build/config
 export default defineConfig({
-    integrations: [icon()],
-    image: {
-        remotePatterns: [{ protocol: "https" }],
-      }
+  site: 'https://jk-astro-blog.netlify.app',
+  integrations: [icon(), sitemap()],
+  image: {
+    remotePatterns: [{
+      protocol: "https"
+    }]
+  }
 });
